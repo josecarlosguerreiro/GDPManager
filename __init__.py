8a2e43e8b9ceaf3a02308a7f4d2882d2888db83f
JoseGuerreiro <joseguerreiro@outlook.com>
2023-07-19 08:05:00 +0100

    port 5000

--- __init__.py
+++ __init__.py
@@ -115,4 +115,4 @@ def register():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
